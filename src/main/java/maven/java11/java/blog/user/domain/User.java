dfe75e263bc3de7e0aaf475abe4ab6aa9c56a8ee
PhamCongChinh <chinhpham1609@gmail.com>
2022-06-29 22:15:15 +0700

    create user

---
+++ src/main/java/maven/java11/java/blog/user/domain/User.java
@@ -0,0 +1,11 @@
+package maven.java11.java.blog.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private long id;
+    private String name;
+}
